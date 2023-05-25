05ce7e3d5f12428a52349540ef70bbde25579297
JuanEstebanGomezGarciaCAMPUS <juanesteban.gomezgarcia@gmail.com>
2023-05-25 15:36:51 -0500

    feat: :sparkles: Se agregaron los ejercicios 2 y 3
    Se agregaron los ejercicios 2 y 3 a el código y se agregó una imagen de fondo

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
+//Primer ejercicio
+
 class Persona {
-    /**
-     * @param {Este parametro indica el nombre de la persona} nombre 
-     * @param {Este parametro indica la edad de la persona} edad 
-     * @param {Este parametro indica el sexo de la persona} sexo 
-     */
     constructor({nombre, edad, sexo}) {
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
     }
 
-    /**
-     ** Este metodo imprime en consola un saludo con el nombre de la persona
-     */
     saludar() {
         let parrafo = `<p>Hola, mi nombre es ${this.nombre}</p>`
         return parrafo;
@@ -36,12 +30,6 @@ class Persona {
 }
 
 class Estudiante extends Persona {
-    /**
-     * @param {Este parametro indica el nombre de la persona} nombre 
-     * @param {Este parametro indica la edad de la persona} edad 
-     * @param {Este parametro indica el sexo de la persona} sexo 
-     * @param {Este parametro indica la carrera de la persona} carrera 
-     */
     constructor({nombre, edad, sexo, carrera}) {
         super({nombre, edad, sexo});
         this.carrera = carrera;
@@ -55,9 +43,9 @@ class Estudiante extends Persona {
 
 const instanciarPersona = (data) => {
     let persona1 = new Persona(data);
-    let resultado1 = document.getElementById('resultado1');
+    let resultado1 = document.getElementById('resultado11');
     resultado1.removeAttribute('style');
-    let parrafo1 = document.getElementById('parrafo1');
+    let parrafo1 = document.getElementById('parrafo11');
     parrafo1.innerHTML = '';
     parrafo1.insertAdjacentHTML('beforeend', persona1.saludar());
     parrafo1.insertAdjacentHTML('beforeend', Persona.esMayordeEdad(persona1.getEdad, persona1.getNombre));
@@ -65,29 +53,164 @@ const instanciarPersona = (data) => {
 
 const instanciarEstudiante = (data) => {
     let estudiante1 = new Estudiante(data);
-    let resultado2 = document.getElementById('resultado2');
+    let resultado2 = document.getElementById('resultado21');
     resultado2.removeAttribute('style');
-    let parrafo2 = document.getElementById('parrafo2');
+    let parrafo2 = document.getElementById('parrafo21');
     parrafo2.innerHTML = '';
     parrafo2.insertAdjacentHTML('beforeend', estudiante1.saludar());
     parrafo2.insertAdjacentHTML('beforeend', estudiante1.estudiar());
 }
 
-let formulario1 = document.getElementById('formulario1');
-let formulario2 = document.getElementById('formulario2');
+let formulario11 = document.getElementById('formulario11');
+let formulario21 = document.getElementById('formulario21');
 
-/**
- * @param {Este evento se encarga de prevenir el comportamiento por defecto del formulario y de obtener los datos del mismo} e
- */
-
-formulario1.addEventListener('submit', (e) => {
+formulario11.addEventListener('submit', (e) => {
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     instanciarPersona(data);
 })
 
-formulario2.addEventListener('submit', (e) => {
+formulario21.addEventListener('submit', (e) => {
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     instanciarEstudiante(data);
+});
+
+//Segundo ejercicio
+
+class Animal {
+    constructor({nombre, edad, sonido}) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sonido = sonido;
+    }
+    hacerSonido() {
+        return `<p>${this.sonido}</p>`;
+    }
+}
+
+class Perro extends Animal {
+    constructor({nombre, edad, sonido, raza}) {
+        super({nombre, edad, sonido});
+        this.raza = raza;
+    }
+    moverCola() {
+        return `<p>El perro está moviendo la cola</p>`;
+    }
+};
+
+let formulario12 = document.getElementById('formulario12');
+let formulario22 = document.getElementById('formulario22');
+
+const instanciarAnimal = (data) => {
+    let animal1 = new Animal(data);
+    let resultado1 = document.getElementById('resultado12');
+    resultado1.removeAttribute('style');
+    let parrafo1 = document.getElementById('parrafo12');
+    parrafo1.innerHTML = '';
+    parrafo1.insertAdjacentHTML('beforeend', animal1.hacerSonido());
+};
+
+const instanciarPerro = (data) => {
+    let perro1 = new Perro(data);
+    let resultado2 = document.getElementById('resultado22');
+    resultado2.removeAttribute('style');
+    let parrafo2 = document.getElementById('parrafo22');
+    parrafo2.innerHTML = '';
+    parrafo2.insertAdjacentHTML('beforeend', perro1.hacerSonido());
+    parrafo2.insertAdjacentHTML('beforeend', perro1.moverCola());
+};
+
+formulario12.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    instanciarAnimal(data);
+});
+
+formulario22.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    instanciarPerro(data);
+});
+
+//Tercer ejercicio
+
+class Figura {
+    constructor({nombre, area = 0}) {
+        this.nombre = nombre;
+        this.area = area;
+    }
+    calcularArea() {
+        return `<p>El área de la figura es ${this.area}</p>`;
+    }
+}
+
+class Circulo extends Figura {
+    constructor({nombre, radio}) {
+        super({nombre});
+        this.radio = radio;
+    }
+    calcularArea() {
+        return `<p>El área del círculo es ${(Math.PI * this.radio * this.radio).toFixed(2)}</p>`;
+    }
+}
+
+class Rectangulo extends Figura {
+    constructor({nombre, largo, ancho}) {
+        super({nombre});
+        this.largo = parseFloat(largo);
+        this.ancho = parseFloat(ancho);
+    }
+    calcularArea() {
+        return `<p>El área del cuadrado es ${(this.largo * this.ancho).toFixed(2)}</p>`;
+    }
+}
+
+let formulario13 = document.getElementById('formulario13');
+let formulario23 = document.getElementById('formulario23');
+let formulario33 = document.getElementById('formulario33');
+
+const instanciarFigura = (data) => {
+    let figura1 = new Figura(data);
+    let resultado1 = document.getElementById('resultado13');
+    resultado1.removeAttribute('style');
+    let parrafo1 = document.getElementById('parrafo13');
+    parrafo1.innerHTML = '';
+    parrafo1.insertAdjacentHTML('beforeend', figura1.calcularArea());
+}
+
+const instanciarCirculo = (data) => {
+    let circulo1 = new Circulo(data);
+    let resultado2 = document.getElementById('resultado23');
+    resultado2.removeAttribute('style');
+    let parrafo2 = document.getElementById('parrafo23');
+    parrafo2.innerHTML = '';
+    parrafo2.insertAdjacentHTML('beforeend', circulo1.calcularArea());
+}
+
+const instanciarRectangulo = (data) => {
+    let rectangulo1 = new Rectangulo(data);
+    let resultado3 = document.getElementById('resultado33');
+    resultado3.removeAttribute('style');
+    let parrafo3 = document.getElementById('parrafo33');
+    parrafo3.innerHTML = '';
+    parrafo3.insertAdjacentHTML('beforeend', rectangulo1.calcularArea());
+}
+
+formulario13.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    instanciarFigura(data);
+});
+
+formulario23.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    instanciarCirculo(data);
+});
+
+formulario33.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    instanciarRectangulo(data);
 });
